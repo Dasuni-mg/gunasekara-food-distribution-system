81d836bd5007b703477b39603db02a9377d8d1c3
DasuniZanoch <mg.dasuninuwanika@gmail.com>
2024-04-23 17:15:40 +0530

    dark login

--- pages/login.js
+++ pages/login.js
@@ -22,49 +22,52 @@ import {
 function Login() {
 
     return (
-        
-        <VStack >
-            <Box w={['full', 'md']}
-                p={[10, 10]}
-                mt={[20, '10vh']}
-                mx='auto'
-                border={['none', '2px']}
-                borderColor={['', 'linkedin.300']}
-                borderRadius={10}>
-                <VStack >
-                    <Wrap>
-                        <WrapItem>
-                            <Avatar size='xl' name='Gunasekara Food Suppliers' src='https://bit.ly/dan-abramov' />
-                        </WrapItem>
-                    </Wrap>
-                    <VStack>
-                        <Heading>
-                            Login
-                        </Heading>
-                        <Text>Enter your username and password</Text>
+        <Box bg="#050c2b" w="full" h="100vh"> {/* Added background color */}
+            <VStack >
+                <Box w={['full', 'md']}
+                    p={[10, 10]}
+                    mt={[20, '10vh']}
+                    mx='auto'
+                    border={['none', '2px']}
+                    borderColor={['', 'linkedin.300']}
+                    borderRadius={10}
+                    bgGradient="linear(to-r, #050c2b, #0c1153)" // Background gradient
+                >
+                    <VStack >
+                        <Wrap>
+                            <WrapItem>
+                                <Avatar size='xl' name='Gunasekara Food Suppliers' src='https://bit.ly/dan-abramov' />
+                            </WrapItem>
+                        </Wrap>
+                        <VStack>
+                            <Heading color="white" >
+                                Login
+                            </Heading >
+                            <Text color="white" >Enter your username and password</Text>
+                        </VStack>
+                        <FormControl>
+                            <FormLabel color="white" >User Name</FormLabel>
+                            <Input type='text' variant='filled' bgColor="blue.50"/>
+
+                            <FormLabel color="white" >Password</FormLabel>
+                            <Input type='password' variant='filled' bgColor="blue.50"/>
+                            <HStack justify='space-between'>
+                                <Checkbox color="white" >Remember me</Checkbox>
+                                <Link color='blue.500' href='#'>Forgot password? </Link>
+                            </HStack>
+                            <Button w='full'
+                                mt={4}
+                                type='login'
+                                colorScheme='linkedin'
+                            >
+                                Login
+                            </Button>
+                        </FormControl>
+
                     </VStack>
-                    <FormControl>
-                        <FormLabel>User Name</FormLabel>
-                        <Input type='text' variant='filled' bgColor="blue.50"/>
-
-                        <FormLabel>Password</FormLabel>
-                        <Input type='password' variant='filled' bgColor="blue.50"/>
-                        <HStack justify='space-between'>
-                            <Checkbox>Remember me</Checkbox>
-                            <Link color='blue.500' href='#'>Forgot password? </Link>
-                        </HStack>
-                        <Button w='full'
-                            mt={4}
-                            type='login'
-                            colorScheme='linkedin'
-                        >
-                            Login
-                        </Button>
-                    </FormControl>
-
-                </VStack>
-            </Box>
-        </VStack>
+                </Box>
+            </VStack>
+        </Box>
     )
 }
 
